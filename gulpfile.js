ff67f14decd2d1b47960026785e29c634dac454f
Richard Jackson <richard@rbjackson.com>
2017-06-20 14:11:39 -0700

    update task runner

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,16 @@ var gulp      = require( 'gulp' );
 var inlineCss = require( 'gulp-inline-css' );
 var prettify  = require( 'gulp-prettify');
 
+
+// leave empty "" or choose between "_blank" or "_self" to add to anchor links; 
+// target attributes already in anchor links will not be overwritten
+var linkTargetAttribute = "_blank";
+
+
+// data attribute, class to be inlined then deleted
+var inlineAttribute = "aria-labelledby";
+
+
 // NOTE:
 // Any images will need to me moved (manually) to the 
 // "build" directory once gulp has been executed
@@ -83,7 +93,57 @@ function setTracking(linkData, utmSource, utmMedium, utmCampaign) {
 }
 
 
+// ux default override; target attribute
+function setTarget(linkData, targetSetting) {
+  var values = ['_blank', '_self'];
+  var pseudoProtocals = ["mailto", "tel", "javascript", "sms", "geo", "market"];
 
+  // check for any target attribute
+  var href = linkData.match('href\=\"(.*?)\"');
+  var attribute = linkData.match('target');
+  var setting = linkData.match('target\=\"(.*?)\"');
+  var value = '_self'; // default
+
+  // is a link...
+  if (href !== null) {
+
+    // ... but not a `pseudo-protocal` link
+    if (pseudoProtocals.indexOf(href[1].match('^[a-zA-Z0-9]+[^:]')[0]) < 0) {
+
+      // target attribute does exist...
+      if (setting !== null) {
+
+        // ... if it's an approved value, then leave as is.
+        if (values.indexOf(setting[1]) > -1) {
+          value = setting[1];
+        }
+      }
+      // set the value...
+      else {
+
+        // ... if it's an apporved value
+        if (values.indexOf(targetSetting) > -1) {
+          value = targetSetting;
+        }
+      }
+
+      // modify attribute
+      if (attribute !== null) {
+
+        return linkData.replace(/target\=\"(.*?)\"/g, function(targetAttr) {
+          return 'target="'+ value +'"';
+        });
+      }
+      // add attribute
+      else {
+
+        return linkData.replace(/href/g, 'target="'+ value +'" href');
+      }
+    }
+  }
+
+  return linkData;
+}
 
 
 // Move styles inline except for styles in a media query
@@ -151,9 +211,145 @@ gulp.task('remove-not-inline-media-query', ['inline'], function(callback) {
   });
 });
 
+// add google analytic parameters to links
+gulp.task('links', ['remove-not-inline-media-query'], function(callback) {
+
+  // loop through all html files
+  glob('./build/*.html', function (err, matches) {
+    if (err) { return console.log(err); }
+
+    var i, count = 0;
+
+    for(i = 0; i < matches.length; i++) {
+
+      // get the wildcard filename
+      var filename = matches[i];
+
+      // enclose the filename inside an IIFE
+      (function(filename) {
+        return fs.readFile(filename, 'utf8', function (err, data) {
+          if (err) { return console.log(err); }
+
+          var utmSource = "", utmMedium = "", utmCampaign = "",
+          utmSourceMatch = data.match('data\-utm\-source\=\"(.*?)\"'),
+          utmMediumMatch = data.match('data\-utm\-medium\=\"(.*?)\"'),
+          utmCampaignMatch = data.match('data\-utm\-campaign\=\"(.*?)\"');
+
+          // get meta data attributes
+          if (utmSourceMatch !== undefined && utmSourceMatch !== null) { utmSource = utmSourceMatch[1]; }
+          if (utmMediumMatch !== undefined && utmMediumMatch !== null) { utmMedium = utmMediumMatch[1]; }
+          if (utmCampaignMatch !== undefined && utmCampaignMatch !== null) { utmCampaign = utmCampaignMatch[1]; }
+          
+          var file = data;
+
+          // if analytics defined
+          if (utmSource !== "" && utmMedium !== "" && utmCampaign !== "") {
+
+            // Links Part 1
+            file = file.replace(/<a[^>]*>/g, function(anchorData) {
+              return setTracking(anchorData, utmSource, utmMedium, utmCampaign);
+            });
+
+            // Links Part 2   // i.e.: bulletproof buttons
+            file = file.replace(/<v[^>]*>/g, function(vectorData) {
+              return setTracking(vectorData, utmSource, utmMedium, utmCampaign);
+            });
+
+          }
+
+          // if target attribute defined
+          if (linkTargetAttribute !== "") {
+
+            file = file.replace(/<a[^>]*>/g, function(anchorData) {
+              return setTarget(anchorData, linkTargetAttribute);
+            });
+
+            file = file.replace(/<v[^>]*>/g, function(vectorData) {
+              return setTarget(vectorData, linkTargetAttribute);
+            });
+          }
+
+          // if analytics defined and target attribute configure is true
+          if ( (utmSource !== "" && utmMedium !== "" && utmCampaign !== "") || linkTargetAttribute !== "") {
+
+            // overwrite email with new modifications
+            fs.writeFile(filename, file, function (err) {
+              if (err) return console.log(err);
+            });
+          }
+
+          // if there is more than one email...
+          count++;
+          if (count >= matches.length) {
+            callback(); // ... continue gulp sequence
+          }
+
+        });
+      })(filename);
+    }
+  });
+});
+
+
+//remove inline attribute removals
+gulp.task('inline-attribute-removal', ['links'], function(callback) {
+  glob('./build/*.html', function (err, matches) {
+    if (err) { return console.log(err); }
+
+    var i,
+      count = 0,
+      attribute = inlineAttribute,
+      attPattern = new RegExp(attribute+'\=\"(.*?)\"','g'),
+      utmPattern = new RegExp('data-utm-.*?=\".*?\"', 'g'),
+      idPattern = new RegExp('data-.+id=\".*?\"', 'g'),
+      metaPattern = new RegExp('\<meta(?: {1,9}|)(?:\/|)\>\n', 'g');
+
+    for(i = 0; i < matches.length; i++) {
+
+      // get the wildcard filename
+      var filename = matches[i];
+
+      // enclose the filename inside an IIFE
+      (function(filename) {
+        return fs.readFile(filename, 'utf8', function (err, data) {
+          if (err) { return console.log(err); }
+
+          var results = data;
+
+          // Step 1: remove all inline `attribute` selectors
+          results = results.replace(attPattern, "");
+
+          // Step 2: remove all GA data-utm attributes
+          results = results.replace(utmPattern, "");
+
+          // Step 3: remove data-*id attributes
+          results = results.replace(idPattern, "");
+
+          // Step 4: remove empty `meta` tag
+          results = results.replace(metaPattern, "");
+
+
+          // overwrite email with new modifications
+          fs.writeFile(filename, results, function (err) {
+            if (err) return console.log(err);
+          });
+
+          // if there is more than one email...
+          count++;
+          if (count >= matches.length) {
+            callback(); // ... continue gulp sequence
+          }
+
+        });
+
+      })(filename);
+    }
+  });
+});
+
 
 // Format/Clean-up html & css
-gulp.task('prettify', ['remove-not-inline-media-query'], function(callback) {
+gulp.task('prettify', ['inline-attribute-removal'], function(callback) {
   return gulp.src( 'build/*.html' )
     .pipe( prettify( {
       indent_size: 2,
@@ -232,71 +428,8 @@ gulp.task('css-selector-per-line', ['prettify'], function(callback) {
 });
 
 
-// add google analytic parameters to links
-gulp.task('links', ['css-selector-per-line'], function(callback) {
-
-  // loop through all html files
-  glob('./build/*.html', function (err, matches) {
-    if (err) { return console.log(err); }
-
-    var i, count = 0;
-
-    for(i = 0; i < matches.length; i++) {
-
-      // get the wildcard filename
-      var filename = matches[i];
-
-      // enclose the filename inside an IIFE
-      (function(filename) {
-        return fs.readFile(filename, 'utf8', function (err, data) {
-          if (err) { return console.log(err); }
-
-          var utmSource = "", utmMedium = "", utmCampaign = "",
-          utmSourceMatch = data.match('data\-utm\-source\=\"(.*?)\"'),
-          utmMediumMatch = data.match('data\-utm\-medium\=\"(.*?)\"'),
-          utmCampaignMatch = data.match('data\-utm\-campaign\=\"(.*?)\"');
-
-          // get meta data attributes
-          if (utmSourceMatch !== undefined && utmSourceMatch !== null) { utmSource = utmSourceMatch[1]; }
-          if (utmMediumMatch !== undefined && utmMediumMatch !== null) { utmMedium = utmMediumMatch[1]; }
-          if (utmCampaignMatch !== undefined && utmCampaignMatch !== null) { utmCampaign = utmCampaignMatch[1]; }
-          
-
-          // if analytics defined
-          if (utmSource !== "" && utmMedium !== "" && utmCampaign !== "") {
-
-            // Links Part 1
-            var linkAnchor = data.replace(/<a[^>]*>/g, function(anchorData) {
-              return setTracking(anchorData, utmSource, utmMedium, utmCampaign);
-            });
-
-            // Links Part 2 // i.e.: bulletproof buttons
-            linkAnchor = linkAnchor.replace(/<v[^>]*>/g, function(vectorData) {
-              return setTracking(vectorData, utmSource, utmMedium, utmCampaign);
-            });
-
-            // overwrite email with new modifications
-            fs.writeFile(filename, linkAnchor, function (err) {
-              if (err) return console.log(err);
-            });
-
-          }
-
-          // if there is more than one email...
-          count++;
-          if (count >= matches.length) {
-            callback(); // ... continue gulp sequence
-          }
-
-        });
-      })(filename);
-    }
-  });
-});
-
-
 // Default: start the task cascade
-gulp.task('default', ['links'], function(callback) {
+gulp.task('default', ['css-selector-per-line'], function(callback) {
 
   // ready, set, go
 
