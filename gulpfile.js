8241a3908466e219776c9b4ee0b119133d269a14
Richard Jackson <richard@rbjackson.com>
2017-06-22 10:19:42 -0700

    update setTarget in links to ignore `#`

--- gulpfile.js
+++ gulpfile.js
@@ -105,7 +105,7 @@ function setTarget(linkData, targetSetting) {
   var value = '_self'; // default
 
   // is a link...
-  if (href !== null) {
+  if (href !== null && href[1].charAt(0) !== '#') {
 
     // ... but not a `pseudo-protocal` link
     if (pseudoProtocals.indexOf(href[1].match('^[a-zA-Z0-9]+[^:]')[0]) < 0) {
